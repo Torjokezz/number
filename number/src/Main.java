48bb04cbf70df1095c92529b23a19dd4d87a5be3
Torjokezz <vladimir272727@yandex.ru>
2022-12-15 17:23:06 +0300

    Scanner Commit

--- number/src/Main.java
+++ number/src/Main.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello JavaWorld!");
+        int x;
+        int max = 3;
+        int min = 1;
+        int num = 0;
+        x = (int)(Math.random() * ((max - min)+1)) + min;
+        System.out.println("Загадано число " + x);
+        Scanner in = new Scanner(System.in);
+
+        while (num != x) {
+            System.out.print("Введите загаданное число: ");
+            num = in.nextInt();
+            if (num < x) {System.out.print("Число меньше");}
+        }
+
+        System.out.printf("Вы угадали. Это число: %d \n", x);
+        in.close();
+        //System.out.println(x);
     }
 
 }
